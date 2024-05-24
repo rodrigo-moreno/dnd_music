f08a97d9b6dcc71431be41dd8a477b945030242a
Rodrigo Moreno <luiromormor@gmail.com>
2024-05-24 17:12:22 +0200

    Implemented predition interpretation instead of denoiser.
    Our previous attempt used the denoiser interpretation to calculate the
    loss and do training. It failed, so this new implementation uses the
    prediction of x0 interpretation.

--- model.py
+++ model.py
@@ -1,7 +1,14 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 
+################################################################################
+################################################################################
+################################################################################
+### EMBEDDINGS
+
+### I feel like this could have been a function instead of a class but whatever
 class TimeEmbd(nn.Module):
     """
     Do the time embedding changing from a scalar value to a vector of size C
@@ -14,17 +21,18 @@ class TimeEmbd(nn.Module):
     Output:
     - vector in R^C represented in [1, C, 1, 1] for convenience.
     """
-
     def __init__(self, channels):
         super().__init__()
         self.C = channels
 
     def forward(self, t):
-        omega = torch.arange(1, self.C // 2 + 1)
-        t = t.unsqueeze(1)
+        omega = torch.arange(1, self.C//2+1)
+        t = t.unsqueeze(0)
+        #print(f'In T. Embd: {t.shape}')
         p1 = (omega * t).sin()
         p2 = (omega * t).cos()
-        t_emb = torch.cat((p1, p2), 1)
+        #print(f'In T. Embd: {p1.shape}')
+        t_emb = torch.concatenate((p1, p2), 0)
         return t_emb.unsqueeze(-1).unsqueeze(-1)
 
 
@@ -33,14 +41,21 @@ class GenreEmbd(nn.Module):
     Do the genre embedding for the network. Contrary to TimeEmbd, this embedding
     IS learnable.
     """
-
     def __init__(self, genres, channels):
         super().__init__()
         self.model = nn.Embedding(genres, channels)
 
     def forward(self, genre):
-        # Reshaped for convenience
+        ### As above, reshaped for convenience
+        tensor = self.model(genre)[:, ..., None, None]
         return self.model(genre)[:, ..., None, None]
+
+
+
+################################################################################
+################################################################################
+################################################################################
+### SUBARCHITECTURES
 
 
 class UNet(nn.Module):
@@ -49,50 +64,59 @@ class UNet(nn.Module):
     Substitutes RecoverX0 written before. Takes xt as input and tries to
     recover x0.
     """
-
     def __init__(self):
         super().__init__()
 
-        # Define model blocks
-        self.fatten = nn.Conv2d(16, 64, kernel_size=3, stride=1, padding=1)
+        ### Define model blocks
+        self.fatten = nn.Conv2d(1, 64, kernel_size=3,
+                                stride=1, padding=1)
         self.down1 = Down(64, 128, 80, 2048)
         self.down2 = Down(128, 256, 40, 1024)
         self.down3 = Down(256, 512, 20, 512)
         self.up1 = Up(512, 256, 10, 256)
         self.up2 = Up(256, 128, 20, 512)
         self.up3 = Up(128, 64, 40, 1024)
-        self.thin = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+        self.thin = nn.Conv2d(64, 1, kernel_size=3,
+                              stride=1, padding=1)
 
-        # Define time embeddings
+        ### Define time embeddings
         self.t1 = TimeEmbd(64)
         self.t2 = TimeEmbd(128)
         self.t3 = TimeEmbd(256)
         self.t4 = TimeEmbd(512)
 
-        # Define genre embedding
+        ### Define genre embedding
         self.g1 = GenreEmbd(8, 64)
         self.g2 = GenreEmbd(8, 128)
         self.g3 = GenreEmbd(8, 256)
         self.g4 = GenreEmbd(8, 512)
 
-        # Apply He initialization
+        ### Apply He initialization of parameters
         self.apply(self._init_weights)
 
     def _init_weights(self, module):
         if isinstance(module, nn.Conv2d):
-            nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="relu")
+            nn.init.kaiming_normal_(module.weight, mode='fan_out',
+                                    nonlinearity='relu')
             if module.bias is not None:
                 nn.init.constant_(module.bias, 0)
 
     def forward(self, x, genre, t):
         x = self.fatten(x)
         d1 = self.down1(x + self.t1(t) + self.g1(genre))
+        #print(f'\tD1: {d1.shape}')
         d2 = self.down2(d1 + self.t2(t) + self.g2(genre))
+        #print(f'\tD2: {d2.shape}')
         d3 = self.down3(d2 + self.t3(t) + self.g3(genre))
+        #print(f'\tD3: {d3.shape}')
         u = self.up1(d3 + self.t4(t) + self.g4(genre))
+        #print(f'\tU1: {u.shape}')
         u = self.up2(u + d2 + self.t3(t) + self.g3(genre))
+        #print(f'\tU2: {u.shape}')
         u = self.up3(u + d1 + self.t2(t) + self.g2(genre))
+        #print(f'\tU3: {u.shape}')
         u = self.thin(u + self.t1(t) + self.g1(genre))
+        #print(f'\tTh: {u.shape}')
         return u
 
 
@@ -101,23 +125,15 @@ class Residual(nn.Module):
     Single residual block inside a UNet Block. Its input is x, and its output
     is the processing of x through model(x) plus x itself.
     """
-
     def __init__(self, in_ch, out_ch, height, width):
         super().__init__()
-        self.model = nn.Sequential(
-            nn.LayerNorm((height, width)),
-            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(out_ch, out_ch, kernel_size=3, stride=1, padding=1),
-        )
-        # Apply He initialization
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Conv2d):
-            nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="relu")
-            if module.bias is not None:
-                nn.init.constant_(module.bias, 0)
+        self.model = nn.Sequential(nn.LayerNorm((height, width)),
+                                   nn.Conv2d(in_ch, out_ch, kernel_size=3,
+                                             stride=1, padding=1),
+                                   nn.ReLU(),
+                                   nn.Conv2d(out_ch, out_ch, kernel_size=3,
+                                             stride=1, padding=1),
+                                   )
 
     def forward(self, x):
         return self.model(x) + x
@@ -133,32 +149,35 @@ class _UNetBlock(nn.Module):
     Whether it is an Up or Down block is determined by the value self.up
     set at instantiation.
     """
-
-    def __init__(self, up: bool):
+    def __init__(self, up:bool):
         super().__init__()
         self.up = up
 
     def _generate_model(self, in_ch, out_ch, height, width):
         """
-        Generate the appropriate model depending on the value of the up flag.
+        Generate the appropriate model depending on the value of the up
+        flag.
         """
         if self.up:
-            model = nn.Sequential(
-                Residual(in_ch, in_ch, height, width),
-                Residual(in_ch, in_ch, height, width),
-                Residual(in_ch, in_ch, height, width),
-                nn.LayerNorm((height, width)),
-                nn.Upsample([height * 2, width * 2], mode="nearest"),
-                nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1),
-                nn.LayerNorm((height * 2, width * 2)),
-            )
+            model = nn.Sequential(Residual(in_ch, in_ch, height, width),
+                                  Residual(in_ch, in_ch, height, width),
+                                  Residual(in_ch, in_ch, height, width),
+                                  nn.LayerNorm((height, width)),
+                                  nn.Upsample([height*2, width*2],
+                                              mode='nearest'),
+                                  nn.Conv2d(in_ch, out_ch, kernel_size=3,
+                                            stride=1, padding=1),
+                                  nn.LayerNorm((height*2, width*2))
+                                  )
+
+
         else:
-            model = nn.Sequential(
-                Residual(in_ch, in_ch, height, width),
-                Residual(in_ch, in_ch, height, width),
-                Residual(in_ch, in_ch, height, width),
-                nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=2, padding=1),
-            )
+            model = nn.Sequential(Residual(in_ch, in_ch, height, width),
+                                  Residual(in_ch, in_ch, height, width),
+                                  Residual(in_ch, in_ch, height, width),
+                                  nn.Conv2d(in_ch, out_ch, kernel_size=3,
+                                            stride=2, padding=1),
+                                  )
         return model
 
     def forward(self, x):
@@ -167,19 +186,12 @@ class _UNetBlock(nn.Module):
 
 class Down(_UNetBlock):
     """
-    Downsampling version of the UNet Block
+    Upsampling version of the UNet Block.
     """
-
     def __init__(self, in_channels, out_channels, height, width):
         super().__init__(up=False)
-        self.model = super()._generate_model(in_channels, out_channels, height, width)
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Conv2d):
-            nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="relu")
-            if module.bias is not None:
-                nn.init.constant_(module.bias, 0)
+        self.model = super()._generate_model(in_channels, out_channels,
+                                             height, width)
 
     def forward(self, x):
         x = self.model(x)
@@ -190,18 +202,229 @@ class Up(_UNetBlock):
     """
     Upsampling version of the UNet Block.
     """
-
     def __init__(self, in_channels, out_channels, height, width):
         super().__init__(up=True)
-        self.model = super()._generate_model(in_channels, out_channels, height, width)
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Conv2d):
-            nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="relu")
-            if module.bias is not None:
-                nn.init.constant_(module.bias, 0)
+        self.model = super()._generate_model(in_channels, out_channels,
+                                             height, width)
 
     def forward(self, x):
         x = self.model(x)
         return x
+
+
+
+################################################################################
+################################################################################
+################################################################################
+### DIFFUSION
+
+class Diffusion(nn.Module):
+    """
+    Doc...
+    """
+    def __init__(self, steps=100, epsilon=0.01):
+        super().__init__()
+        self.steps = steps
+        self.tspan = torch.linspace(0, 1, steps)
+        self.epsilon = torch.tensor(epsilon)
+        #self.up = nn.Sequential([Up(...) for ii in range(depth)])
+        self.up = UNet()
+        self.loss = nn.MSELoss()
+
+    def noise(self, x):
+        """
+        The whole forward steps for the noise.
+        """
+        z = torch.randn_like(x)
+        #mu = torch.prod(torch.tensor([self._mu(t) for t in self.tspan]))
+        mu = self.alpha_bar()
+        #var = torch.prod(torch.tensor([self._var(t) for t in self.tspan]))
+        var = 1 - self.alpha_bar()
+        x_noisy = self._mu(1)*x + self._var(1)*z
+        return x_noisy
+
+    def alpha(self, t):
+        return 1 - 0.9*t
+        #return torch.cos(torch.acos(torch.sqrt(self.epsilon))*t)**2
+
+    def alpha_bar(self, end=None):
+        if end is None:
+            end = self.tspan[-1] + 1
+        return torch.tensor([self.alpha(t) for t in self.tspan
+                             if t <= end]).prod()
+
+    def _mu(self, t_final):
+        return (torch.sqrt(self.epsilon).arccos() * t_final).cos() ** 2
+        #return self.alpha(t_final).sqrt()
+
+    def _var(self, t_final):
+        return (1 - t_final + (self.epsilon.pow(2))).sqrt()
+        #return (1.01 - self.alpha(t_final)).sqrt()
+
+    def forward(self, x, y):
+        ### All the way down in one step for the noisy
+        x_noisy = self.noise(x)
+        #print(f'\t\tNoisy mean: {x_noisy.mean().item()}')
+        L = None
+
+        ### Step by step up for the denoising
+        #print(self.tspan)
+        for t in torch.linspace(1, 0, self.steps)[:-1]:
+            #print(f'\n\tDepth: {t}')
+            pred = self.up(x_noisy, y, t)
+            e = torch.randn_like(pred)
+            #print(f'Shape of prediction: {pred.shape}')
+            #print(f'Shape of target: {x.shape}')
+            loss = self.loss(pred, x)
+            #print(f'\t\tLoss: {loss.item()}')
+            if L is None:
+                L = loss
+            else:
+                L += loss
+            mu = self.mu_up(x_noisy, pred, t)
+            var = self.var_up(t)
+            x_noisy = mu + torch.randn_like(x_noisy)*var
+            #print(f'\t\tNoisy mean: {x_noisy.mean().item()}')
+
+        #loss = (x - x_noisy).mean()**2
+        #print(loss)
+        #L += loss
+        return x_noisy, L
+
+    def mu_up(self, xt, x0, t):
+        #alpha_bar = self.alpha_bar(t)
+        #alpha_bar_prev = self.alpha_bar(t-1)
+        #alpha = self.alpha(t)
+        alpha_bar = self._mu(t).pow(2)
+        alpha_bar_prev = self._mu(t - 1).pow(2)
+        alpha = alpha_bar / alpha_bar_prev
+        summand1 = ((alpha.sqrt() * (1 - alpha_bar_prev) * xt) /
+                    (1 - alpha_bar))
+        summand2 = ((alpha_bar_prev.sqrt() * (1 - alpha) * x0) /
+                    (1 - alpha_bar))
+        #print(f'\t\t\tmu: {(summand1 + summand2).mean()}')
+        return summand1 + summand2
+
+    def var_up(self, t):
+        #alpha = self.alpha(t)
+        #alpha_bar_prev = self.alpha_bar(t-1)
+        #alpha_bar = self.alpha_bar(t)
+        alpha_bar = self._mu(t).pow(2)
+        alpha_bar_prev = self._mu(t - 1).pow(2)
+        alpha = alpha_bar / alpha_bar_prev
+        #print(f'alpha_bar: {alpha_bar}\nalpha_bar_p: {alpha_bar_prev}\nalpha: {alpha}')
+        var_sq = (1 - alpha)*(1 - alpha_bar_prev) / (1 - alpha_bar)
+        #print(f'\t\t\tvar: {var_sq}')
+        if var_sq == 0:
+            print('Variance has reached 0.')
+        return var_sq
+
+    @torch.no_grad()
+    def sample(self, x, g):
+        """
+        Sampling function for the Diffusion model. Takes a random array and a
+        genre determiner, and returns the final image.
+        """
+        ### Set all inputs to the correct shape
+        if not isinstance(g, torch.Tensor):
+            g = torch.tensor([g])
+        if 80 not in x.shape or 2048 not in x.shape:
+            raise
+        if len(x.shape) != 4:
+            x = x.view(1, 1, 80, 2048)
+
+        ### Step by step up for the denoising
+        ### Equivalent to self.forward without generation of noisy and error
+        ### calculation
+        for t in torch.linspace(1, 0, self.steps)[:-1]:
+            pred = self.up(x_noisy, y, t)
+            e = torch.randn_like(pred)
+            mu = self.mu_up(x_noisy, pred, t)
+            var = self.var_up(t)
+            x_noisy = mu + torch.randn_like(x_noisy)*var
+        return x_noisy
+
+
+def KS(a, b):
+    """
+    Function...
+    """
+    return 1
+
+
+
+
+
+################################################################################
+################################################################################
+################################################################################
+### IMPLEMENTATION
+
+if __name__ == '__main__':
+    ### Checks if everything works correctly
+
+    ### Check if Genre Emedding works
+    if False:
+        model = GenreEmbd(3, 2)
+        g1 = torch.tensor([1])
+        print(g1.shape)
+        print(model(g1).shape)
+        g2 = torch.tensor([0, 1, 2, 2])
+        print(g2.shape)
+        print(model(g2).shape)
+        exit()
+
+    ### Check if Time Embedding works
+    if False:
+        model = TimeEmbd(3)
+        t = torch.linspace(0, 1, 90)
+        ts = t[3]
+        print(ts.shape)
+        print(model(ts).shape)
+        exit()
+
+    ### Check if UNet works
+    if False:
+        x = torch.randn(4, 1, 80, 2048)
+        y = torch.randint(0, 7, [4, ])
+        print(y)
+        print(f'In: {x.shape}')
+        model = UNet()
+        out = model(x, y, torch.tensor(0.5))
+        print(f'Out: {out.shape}')
+        exit()
+
+    ### Check if Diffusion works
+    if False:
+        x = torch.randn(4, 1, 80, 2048)
+        y = torch.randint(0, 7, [4, ])
+        print(y)
+        print(f'In: {x.shape}')
+        D = Diffusion(3)
+        desc = 'Model with {} parameters and {} trainable parameters'
+        print(desc.format(sum(p.numel() for p in D.parameters()),
+                          sum(p.numel() for p in D.parameters()
+                              if p.requires_grad)))
+        out, error = D(x, y)
+        print(f'Final: {out.shape} with error {error}')
+        error.backward()
+        print(next(iter(D.parameters())).grad.mean())
+        exit()
+
+    ### Check if sampling works
+    if True:
+        model = Diffusion(10)
+        if False:
+            ### Check if it works with the desired type of input
+            x = torch.randn(1, 1, 80, 2048)
+            g = torch.tensor([1])
+            pred = model.sample(x, g)
+            print(pred.shape)
+            exit()
+        if True:
+            ### Check if it works with weird inputs
+            x = torch.randn(80, 2048)
+            g = 1
+            out = model.sample(x, g)
+            print(out.mean())
+
--- training.py
+++ training.py
@@ -2,9 +2,8 @@ import os
 import torch
 import wandb
 from torch.utils.data import DataLoader
-from model import UNet
+from model import Diffusion
 from dataset import MelSpectrogramDataset
-from diffusion import DiffusionModel
 from tqdm import tqdm
 from torch.optim.lr_scheduler import StepLR
 
@@ -25,105 +24,63 @@ def train_model(device, learning_rate, weight_decay):
     """
     image_dir = "data"
     num_epochs = 15
-    batch_size = 16
-    num_timesteps = 1000
+    batch_size = 4
+    num_timesteps = 10
 
     # Load the dataset and create a dataloader
     dataset = MelSpectrogramDataset(image_dir)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True,
+                            num_workers=4)
 
     # Initialize the model, optimizer, scheduler, and diffusion model
-    model = UNet().to(device)
-    optimizer = torch.optim.Adam(
-        model.parameters(), lr=learning_rate, weight_decay=weight_decay
-    )
+    model = Diffusion(num_timesteps).to(device)
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=learning_rate,
+                                 weight_decay=weight_decay
+                                )
     scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
-    diffusion = DiffusionModel(num_timesteps=num_timesteps)
 
     # Initialize wandb
     wandb.init(project="dnd_music diffusion model")
 
     residual_images = []
-
-    # Training loop
     for epoch in range(num_epochs):
         epoch_loss = 0
         for images, label in tqdm(dataloader):
-            if images.size(0) != batch_size:
+            if images.shape[0] != batch_size:
                 residual_images.append(images)
-                print(f"Skipping batch with size {images.size(0)}")
+                print(f'Skipping batch with size {images.shape[0]}')
                 continue
 
             images = images.to(device)
 
-            t = torch.randint(0, num_timesteps, (images.size(0),), device=device).long()
-            noise = torch.randn_like(images).to(device)
-
+            ### This model is now directly the diffusion model
+            out, loss = model(images, label)
             optimizer.zero_grad()
-            x_t = diffusion.q_sample(images, t, noise)
-            predicted_noise = model(x_t, label.int(), t)
-            loss = torch.mean((noise - predicted_noise) ** 2)
-
             loss.backward()
             optimizer.step()
 
             epoch_loss += loss.item()
 
         epoch_loss /= len(dataloader)
-        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss:.4f}")
-        torch.save(model.state_dict(), f"steps/model_{epoch + 1}.15.pth")
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}')
+        torch.save(model.state_dict(), f'steps/model_{epoch+1}_R.pth')
 
-        # Log the loss to wandb
-        wandb.log({"epoch": epoch + 1, "loss": epoch_loss})
-        wandb.watch(model, log="all")
+        wandb.log({'loss': epoch_loss})
+        wandb.watch(model, log='all')
 
-        # Step the learning rate scheduler
         scheduler.step()
-
-    torch.save(model.state_dict(), f"diffusion_model_{learning_rate}{weight_decay}.pth")
-
-
-def sweep_train():
-    """
-    Function to train the model with parameters from wandb sweep.
-    """
-    with wandb.init() as run:
-        config = run.config
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        train_model(device, config.learning_rate, config.weight_decay)
+    torch.save(model.state_dict(), f'final_state_R.')
 
 
 def main():
-    """
-    Main function to set up the environment and start training and testing.
-    """
-    path_to_key = "../../wandb_key.txt"
-    key = open(path_to_key, "r").read().split("\n")[0]
-    wandb.login(key=key)
-
-    os.makedirs("steps", exist_ok=True)
-
-    # Configure wandb sweep
-    # sweep_config = {
-    #    'method': 'grid',
-    #    'parameters': {
-    #        'learning_rate': {
-    #            'values': [1e-4, 1e-5]
-    #        },
-    #        'weight_decay': {
-    #            'values': [1e-5, 1e-6]
-    #        }
-    #    }
-    # }
-
-    # sweep_id = wandb.sweep(sweep_config, project="dnd_music diffusion model")
-    # wandb.agent(sweep_id, function=sweep_train)
+    os.makedirs('steps', exist_ok=True)
 
     lr = 1e-3
     wd = 1e-5
-    global_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    global_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     train_model(global_device, lr, wd)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
